83186132222d67653b33796831a12cee53a8439f
oykj <2058893105@qq.com>
2021-01-23 18:10:58 +0800

    添加test.java文件

--- src/main/java/com/xxxx/Test01.java
+++ src/main/java/com/xxxx/Test01.java
@@ -1,6 +1,6 @@
 package com.xxxx;
 
-public class test {
+public class Test01 {
     public static void main(String[] args) {
         System.out.println("test01");
         System.out.println("test01");
