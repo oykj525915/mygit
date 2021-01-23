585d839ec12f5f56f8c5fe104b8b2d843b492ccd
oykj <2058893105@qq.com>
2021-01-23 15:20:35 +0800

    修改test.java

--- src/main/java/com/xxxx/test.java
+++ src/main/java/com/xxxx/test.java
@@ -3,5 +3,7 @@ package com.xxxx;
 public class test {
     public static void main(String[] args) {
         System.out.println("test01");
+        System.out.println("test01");
+        System.out.println("test01");
     }
 }
